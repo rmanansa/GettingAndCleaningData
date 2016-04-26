347f8086ae526f694b000db6a1e23d6ac978c434
Ramil Manansala <rmanansala@castlighthealth.com>
2016-04-26 00:58:15 -0700

    final implementation of run_analyis.R script plus the resulting tidy data

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,37 @@
-# run_analysis.R
+if (!file.exists("./data")){dir.create("./data")}
+zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(zipUrl, destfile = "./data/Dataset.zip",method="curl")
+unzip("./data/Dataset.zip")
+## Merging of data sets
+features <- read.table("./data/UCI HAR Dataset/features.txt")
+X_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt", col.names = features[,2])
+X_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt", col.names = features[,2])
+mergedX <- rbind(X_test, X_train)
+## Getting Mean and standard deviation measurements
+featureMeanStd <- features[grep("(mean|std)\\(", features[,2]),]
+meanAndStd <- mergedX[, featureMeanStd[,1]]
+
+## Use descriptive activity names to name the activities in the data set
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt", col.names = c('activities'))
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt", col.names = c('activities'))
+mergedY <- rbind(y_test, y_train)
+
+activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+
+for (i in 1:nrow(activity_labels)) {
+  key <- as.numeric(activity_labels[i,1])
+  val <- as.character(activity_labels[i,2])
+  mergedY[mergedY$activities == key, ] <- val
+}
+
+## Put appropriate labels to the data set with descriptive activity names
+labeledX <- cbind(mergedY, mergedX)
+labeledMeanAndStd <- cbind(mergedY, meanAndStd)
+
+## Create another independent tidy data set with the average of each 
+## variable for each activity and each subject.
+testSubject <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", col.names = c('Subject'))
+trainSubject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", col.names = c('Subject'))
+mergedSubjectTestTrain <- rbind(testSubject, trainSubject)
+tidyData <- aggregate(mergedX, by = list(Activities = mergedY[,1], Subject = mergedSubjectTestTrain[,1]), mean)
+write.table(tidyData, file = "./data/GettingAndCleaningData/tidy_data.txt", row.names = FALSE)
